f9a6297b09d4b52bd7a1b7920f2ea932b09beecd
phukiendanang01-png <phukiendanang01@gmail.com>
2026-07-11 10:22:48 +0700

    Update: Real products - OPPO Pad Mini + iPhone 11

---
+++ js/products.js
@@ -0,0 +1,22 @@
+const PRODUCTS = [
+ {
+ id: 1,
+ name: "OPPO Pad Mini 256G Fullbox",
+ category: "phone",
+ specs: "256GB | Màu Xám",
+ price: 11750000,
+ image: ""
+ },
+ {
+ id: 2,
+ name: "iPhone 11 VN/A 64G Zin 100%",
+ category: "phone",
+ specs: "64GB | Trắng",
+ price: 3900000,
+ image: ""
+ }
+];
+
+function formatPrice(price) {
+ return new Intl.NumberFormat("vi-VN").format(price) + "₫";
+}
